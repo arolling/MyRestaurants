2896739e5aac3cfb87fc7bc4373ee5e0bc959f36
Abigail Rolling <arolling@gmail.com>
2016-04-18 21:45:33 -0700

    create arrayadapter and set listview adapter

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -3,11 +3,19 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
     private TextView mLocationTextView;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,9 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
