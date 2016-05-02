1ca4a30eb08d7f09955753395e3a743ce0430bde
Abigail Rolling <arolling@gmail.com>
2016-05-02 08:28:05 -0700

    added firebase information and dependencies as per lesson

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
     public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
 
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 }
